37ee748b76c0d3d4a8b226903ea196882d6c4dd0
Omid <omidksand@gmail.com>
2020-11-05 09:45:15 -0500

    Add global error handler and not found routes.

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ const routes = require('./rotues');
    /** Setup API Rotues */
    routes(app, urlShorten);
 
+   /** Error: Not Found (404) */
+   app.use((req, res) => {
+      res.json({
+         success: false,
+         error: '404, Invalid reuested.'
+      });
+   });
+
+   /** Global Error Handler */
+   app.use((err, req, res, next) => {
+      res.json({
+         success: false,
+         error: '500, Internal error.'
+      });
+      console.error('*** Server Error Log  ***', err);
+   });
+
    /** Initiate API Server. */
    app.listen(CONFIG.PORT, async () => {
       console.log(`Url Shorten running on port ${CONFIG.PORT}`);
